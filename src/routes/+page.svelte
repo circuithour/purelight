52d28630324d2a52e72f7d8133f6c79de6049613
circuithour <circuithour@gmail.com>
2023-08-26 00:37:27 -0400

    The Third Commit :O

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <h1>Poop Sock!!!!</h1>
+</main>
+
+<style>
+    h1 {
+        font-size: 200%;
+        color: blue;
+        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    }
+
+
+</style>
